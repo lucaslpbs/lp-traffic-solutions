a53875b476a8d41fe1b30db3e91f2eab889d4a9f
CIEL085 <gabrielpaulinociel9000@gmail.com>
2026-05-20 18:49:29 -0300

    resolvido conflito

--- src/integrations/supabase/clientGestao.ts
+++ src/integrations/supabase/clientGestao.ts
@@ -4,12 +4,6 @@ const SUPABASE_GESTAO_URL = import.meta.env.VITE_SUPABASE_GESTAO_URL;
 const SUPABASE_GESTAO_KEY = import.meta.env.VITE_SUPABASE_GESTAO_KEY;
 
 export const supabaseGestao = createClient(SUPABASE_GESTAO_URL, SUPABASE_GESTAO_KEY, {
-  global: {
-    headers: {
-      apikey: SUPABASE_GESTAO_KEY,
-      Authorization: `Bearer ${SUPABASE_GESTAO_KEY}`,
-    }
-  },
   auth: {
     persistSession: false,
     autoRefreshToken: false,
