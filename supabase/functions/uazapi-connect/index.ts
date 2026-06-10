4a027aef01adc4c196f3f012bd215ee8bfd5108b
CIEL085 <gabrielpaulinociel9000@gmail.com>
2026-06-10 17:17:24 -0300

    ultimas configs e cors

--- supabase/functions/uazapi-connect/index.ts
+++ supabase/functions/uazapi-connect/index.ts
@@ -1,6 +1,7 @@
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
+  "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+  "Access-Control-Allow-Headers": "authorization, apikey, content-type",
 };
 
 const jsonResponse = (status: number, data: unknown) =>
